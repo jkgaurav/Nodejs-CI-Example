61a4fde0b6a2c6a90b09eb23cc16cee1adfa4914
jkgaurav <50937472+jkgaurav@users.noreply.github.com>
2024-01-11 22:46:56 +0530

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,18 +1,31 @@
-// Example test file: app.test.js
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../app');
+const { expect } = chai;
 
 chai.use(chaiHttp);
-chai.should();
 
 describe("GET /", () => {
+  let server;
+
+  // Start the server before the tests
+  before((done) => {
+    server = app.listen(3000, () => {
+      done();
+    });
+  });
+
+  // Stop the server after the tests
+  after(() => {
+    server.close();
+  });
+
   it("should return Hello, GitHub Actions!", (done) => {
-    chai.request(app)
+    chai.request(server)
       .get('/')
       .end((err, res) => {
-        res.should.have.status(200);
-        res.text.should.equal('Hello, GitHub Actions!');
+        expect(res).to.have.status(200);
+        expect(res.text).to.equal('Hello, GitHub Actions!');
         done();
       });
   });
